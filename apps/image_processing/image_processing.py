f9ac3c0b1e1f040ea17b348b623184f474e9f4b5
zhiqi bao <zhiqibao@zhiqideMacBook-Pro-2.local>
2020-07-24 10:53:22 +0800

    embd cal: x_aligned format change

--- apps/image_processing/image_processing.py
+++ apps/image_processing/image_processing.py
@@ -381,7 +381,7 @@ class CalFaceEmbd(BaseHandler):
                 print('align - process: %s/%s'%(count, loader_length))
             count += 1
             #x_aligned, prob = mtcnn(x, return_prob=True)
-            x_aligned = [transforms.ToTensor()(tmp_x).unsqueeze_(0) for tmp_x in x]
+            x_aligned = [transforms.ToTensor()(tmp_x) for tmp_x in x]
             if x_aligned is not None:
                 aligned.extend(x_aligned)
                 names.extend([dataset.idx_to_class[i] for i in y])
